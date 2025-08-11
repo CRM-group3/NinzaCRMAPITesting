5e7388c9e1f0396b4f6a8c3f5c9d51d9c5f83a25
CRM-group3 <tekarchgroup3@gmail.com>
2025-08-11 10:02:07 -0700

    first api commit gp3

---
+++ tests/leadtests/leadapitests.js
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+import { leadrequest1} from '../test-data/leadrequests';
+
+test.describe('Lead API Tests', async (request)=>{
+
+    test('lead id should be unique', async ({ page }) => {
+
+    });
+
+
+})
+
