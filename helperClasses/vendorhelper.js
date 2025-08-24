172ce0cb439a1cdec2a448da31f10ba07217834a
CRM-group3 <tekarchgroup3@gmail.com>
2025-08-23 19:20:41 -0700

    update

--- helperClasses/vendorhelper.js
+++ helperClasses/vendorhelper.js
@@ -57,11 +57,11 @@ class VendorHelper {
   // }
   static async getConnection() {
     return mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: process.env.DB_PORT
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      port: process.env.MYSQL_PORT
     });
   }
 
